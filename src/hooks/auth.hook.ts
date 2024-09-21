4aea61b6a22375462d81b280cd3e8e4e896579b9
Shah Alam <shahalam01660166344@gmail.com>
2024-09-21 19:00:34 +0600

    added jwt-decoded

--- src/hooks/auth.hook.ts
+++ src/hooks/auth.hook.ts
@@ -1,7 +1,7 @@
 import { useMutation } from "@tanstack/react-query";
 import { FieldValues } from "react-hook-form";
-import registerUser from "../services/AuthServices";
 import { toast } from "sonner";
+import { loginUser, registerUser } from "../services/AuthServices";
 
 export const useUserRegistration = () => {
   return useMutation<any, Error, FieldValues>({
@@ -9,6 +9,19 @@ export const useUserRegistration = () => {
     mutationFn: async (userData) => await registerUser(userData),
     onSuccess: () => {
       toast.success("User registration successful.");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+};
+
+export const useUserLogin = () => {
+  return useMutation<any, Error, FieldValues>({
+    mutationKey: ["USER_LOGIN"],
+    mutationFn: async (userData) => await loginUser(userData),
+    onSuccess: () => {
+      toast.success("User login successful.");
     },
     onError: (error) => {
       toast.error(error.message);
--- src/services/AuthServices/index.ts
+++ src/services/AuthServices/index.ts
@@ -3,8 +3,9 @@
 import AxiosInstance from "@/src/lib/AxiosInstance";
 import { cookies } from "next/headers";
 import { FieldValues } from "react-hook-form";
+import { jwtDecode } from "jwt-decode";
 
-const registerUser = async (UserData: FieldValues) => {
+export const registerUser = async (UserData: FieldValues) => {
   try {
     const { data } = await AxiosInstance.post("/auth/register", UserData);
 
@@ -19,4 +20,43 @@ const registerUser = async (UserData: FieldValues) => {
   }
 };
 
-export default registerUser;
+export const loginUser = async (userData: FieldValues) => {
+  try {
+    const { data } = await AxiosInstance.post("/auth/login", userData);
+
+    if (data.success) {
+      cookies().set("accessToken", data?.data?.accessToken);
+      cookies().set("refreshToken", data?.data?.refreshToken);
+    }
+
+    return data;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
+
+export const logout = () => {
+  cookies().delete("accessToken");
+  cookies().delete("refreshToken");
+};
+
+export const getCurrentUser = async () => {
+  const accessToken = cookies().get("accessToken")?.value;
+
+  let decodedToken = null;
+
+  if (accessToken) {
+    decodedToken = await jwtDecode(accessToken);
+    return {
+      _id: decodedToken._id,
+      name: decodedToken.name,
+      email: decodedToken.email,
+      mobileNumber: decodedToken.mobileNumber,
+      role: decodedToken.role,
+      status: decodedToken.status,
+      profilePhoto: decodedToken.profilePhoto,
+    };
+  }
+
+  return decodedToken;
+};
